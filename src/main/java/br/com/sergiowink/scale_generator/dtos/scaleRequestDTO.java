c95a94f92184765a87d4e4262a004ce950517b84
sergio <winksousa0@gmail.com>
2024-01-23 15:59:24 -0300

    add corsFilter

--- src/main/java/br/com/sergiowink/scale_generator/dtos/scaleRequestDTO.java
+++ src/main/java/br/com/sergiowink/scale_generator/dtos/scaleRequestDTO.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class scaleRequestDTO {
     
     private String title;
-
+ 
     private ArrayList<String> names;
 
     private ArrayList<String> daysOfWeek;
---
+++ src/main/java/br/com/sergiowink/scale_generator/providers/CorsFilter.java
@@ -0,0 +1,44 @@
+package br.com.sergiowink.scale_generator.providers;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest; 
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req; 
+        jakarta.servlet.http.HttpServletResponse response = (jakarta.servlet.http.HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(jakarta.servlet.http.HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
